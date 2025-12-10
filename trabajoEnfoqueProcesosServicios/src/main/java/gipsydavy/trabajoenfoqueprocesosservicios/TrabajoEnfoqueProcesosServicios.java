b127c9510f81ab439c5faf884fca60f463d28175
Gipsybuho <gipsybuho@gmail.com>
2025-12-10 20:21:09 +0100

    actualizacion

--- trabajoEnfoqueProcesosServicios/src/main/java/gipsydavy/trabajoenfoqueprocesosservicios/TrabajoEnfoqueProcesosServicios.java
+++ trabajoEnfoqueProcesosServicios/src/main/java/gipsydavy/trabajoenfoqueprocesosservicios/TrabajoEnfoqueProcesosServicios.java
@@ -1,16 +1,13 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package gipsydavy.trabajoenfoqueprocesosservicios;
 
 /**
  *
- * @author Gipsy Dávy
+ * @author Gipsy Buho
  */
 public class TrabajoEnfoqueProcesosServicios {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) {     
+        
+       
     }
 }
